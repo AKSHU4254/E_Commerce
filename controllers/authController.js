e33efde593ee5db239b208e6f761f4c5cf818b5f
AKSHU4254 <Akshaybauskar47@gmail.com>
2023-09-26 10:42:17 +0530

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,269 @@
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import userModal from "../models/userModal.js";
+import orderModel from "../models/orderModel.js"
+import JWT from "jsonwebtoken";
+
+const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address, answer } = req.body;
+    // validation
+    if (!name) {
+      return res.send({ message: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "password is Required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone No. is Required" });
+    }
+    if (!address) {
+      return res.send({ message: "Address is Required" });
+    }
+    if (!answer) {
+      return res.send({ message: "Answer is Required" });
+    }
+
+    // check user
+    const exisitingUser = await userModal.findOne({ email });
+    // exisiting user
+    if (exisitingUser) {
+      return res.send(200).send({
+        success: false,
+        message: "Already Register Please Login",
+      });
+    }
+
+    // register user
+    const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModal({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+      answer,
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registeration",
+      error,
+    });
+  }
+};
+
+// Login
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    // check user
+    const user = await userModal.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registerd",
+      });
+    }
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Passward",
+      });
+    }
+    //token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "Login successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+        role: user.role,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      Message: "Error in login",
+      error,
+    });
+  }
+};
+
+// Forgot Password
+const ForgotPassswordController = async (req, res) => {
+  try {
+    const { email, answer, newPassword } = req.body;
+    if (!email) {
+      res.status(400).send({ message: "Email is Required" });
+    }
+    if (!answer) {
+      res.status(400).send({ message: "answer is Required" });
+    }
+    if (!newPassword) {
+      res.status(400).send({ message: "New Password is Required" });
+    }
+    // check
+    const user = await userModal.findOne({ email, answer });
+    //validation
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Wrong Email Or Answer",
+      });
+    }
+    const hashed = await hashPassword(newPassword);
+    await userModal.findByIdAndUpdate(user._id, { password: hashed });
+    res.status(200).send({
+      success: true,
+      message: "Password Reset Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      Message: "Error in login",
+      error,
+    });
+  }
+};
+
+//test controller
+const testController = async (req, res) => {
+  try {
+    res.send("Protected Routes");
+  } catch (error) {
+    console.log(error);
+    res.send({ error });
+  }
+};
+
+// update profile
+
+const updateProfileController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+    const user = await userModal.findById(req.user._id);
+    // password
+    if (password && password.length < 6) {
+      return res.json({ error: "Password is required and 6 character long" });
+    }
+    const hashedPassword = password ? await hashPassword(password) : undefined;
+    const updatedUser = await userModal.findByIdAndUpdate(
+      req.user._id,
+      {
+        name: name || user.name,
+        password: hashedPassword || user.password,
+        phone: phone || user.phone,
+        address: address || user.address,
+      },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Profile Updated Successfully",
+      updatedUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error while Updaing prfile",
+      error,
+    });
+  }
+};
+
+//orders
+ const getOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({ buyer: req.user._id })
+      .populate("products", "-photo")
+      .populate("buyer", "name");
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Geting Orders",
+      error,
+    });
+  }
+};
+
+// all orders
+ const getAllOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({})
+      .populate("products", "-photo")
+      .populate("buyer", "name")
+      .sort({ createdAt: "-1" });
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error WHile Geting Orders",
+      error,
+    });
+  }
+};
+
+//order status
+ const orderStatusController = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+    const orders = await orderModel.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Updateing Order",
+      error,
+    });
+  }
+};
+
+export {
+  registerController,
+  loginController,
+  testController,
+  ForgotPassswordController,
+  updateProfileController,
+  getOrdersController,
+  getAllOrdersController,
+  orderStatusController,
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan';
+import connestDB from './config/db.js';
+import authRoute from './routes/authRoute.js'
+import categoryRoutes from './routes/categoryRoutes.js'
+import cors from 'cors'
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config();
+
+connestDB();
+const app = express()
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+//routes
+app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/category', categoryRoutes)
+app.use('/api/v1/product', productRoutes)
+
+
+app.get('/', ( req,res ) => {
+    res.send(`<h1> Welcome to application </h1>`)
+})
+
+
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`server Running ${process.env.DEV_MODE} mode on port ${PORT}`)
+})
